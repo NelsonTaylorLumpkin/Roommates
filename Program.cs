d6059b43fab52e96fb2b39ae557d67e2660fb97d
Nelson Taylor Lumpkin <ossocasa@gmail.com>
2023-03-20 13:35:47 -0500

    Second commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Roommates.Models;
+﻿
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
 using Roommates.Repositories;
 using System;
 using System.Collections.Generic;
@@ -14,34 +16,94 @@ namespace Roommates
         static void Main(string[] args)
         {
             RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+            ChoreRepository choreRepo = new ChoreRepository(CONNECTION_STRING);
+            RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
             bool runProgram = true;
             while (runProgram)
             {
                 string selection = GetMenuSelection();
 
                 switch (selection)
+
                 {
-                    case ("Show all rooms"):
+                    case ("Show rooms"):
                         List<Room> rooms = roomRepo.GetAll();
                         foreach (Room r in rooms)
                         {
-                            Console.WriteLine($"{r.Name} has an Id of {r.Id} and a max occupancy of {r.MaxOccupancy}");
+                            Console.WriteLine($"{r.Name} has room Id {r.Id} with a max occupancy of {r.MaxOccupancy} people");
+                        }
+                        Console.Write("press a button to move on");
+                        Console.ReadKey();
+                        break;
+                    case ("Search rooms"):
+                        Console.Write("Room Id: ");
+                        int id = int.Parse(Console.ReadLine());
+                        Room room = roomRepo.GetById(id);
+                        Console.WriteLine($"{room.Id}: {room.Name} MaxOccupancy({room.MaxOccupancy})");
+                        Console.Write("Press a key to continue on");
+                        Console.ReadKey();
+                        break;
+                    case ("Add a room"):
+                        Console.Write("Room name: ");
+                        string RoomName = Console.ReadLine();
+
+                        Console.Write("Max occupancy: ");
+                        int max = int.Parse(Console.ReadLine());
+
+                        Room roomToAdd = new Room()
+                        {
+                            Name = RoomName,
+                            MaxOccupancy = max
+                        };
+
+                        roomRepo.Insert(roomToAdd);
+
+                        Console.WriteLine($"{roomToAdd.Name} has been added and assigned an Id of {roomToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
+                        break;
+                    case ("Show all chores"):
+                        List<Chore> chores = choreRepo.GetAll();
+                        foreach (Chore c in chores)
+                        {
+                            Console.WriteLine($"{c.Name} has an Id of {c.Id}");
                         }
                         Console.Write("Press any key to continue");
                         Console.ReadKey();
                         break;
-                    case ("Search for room"):
-                        // Do stuff
+                    case ("Search For Chore"):
+                        Console.Write("Chore Id: ");
+                        int choreId = int.Parse (Console.ReadLine());
+                        Chore chore = choreRepo.GetById(choreId);
+                        Console.WriteLine($"{chore.Id} - {chore.Name}");
+                        Console.Write("Press Any Key To Continue");
+                        Console.ReadKey ();
                         break;
-                    case ("Add a room"):
-                        // Do stuff
-                        break;
-                    case ("Exit"):
-                        runProgram = false;
+
+                    case ("Add a chore"):
+                        Console.Write("Chore Assignment: ");
+                        string ChoreName = Console.ReadLine();
+
+
+
+                        Chore choreToAdd = new Chore()
+                        {
+                            Name = ChoreName,
+
+                        };
+
+                        choreRepo.Insert(choreToAdd);
+
+                        Console.WriteLine($"{choreToAdd.Name} has been added and assigned an Id of {choreToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
                 }
             }
+           
+               
 
+            
         }
 
         static string GetMenuSelection()
@@ -53,6 +115,13 @@ namespace Roommates
                 "Show all rooms",
                 "Search for room",
                 "Add a room",
+                "Show all chores",
+                "Search for chore",
+                "Show unassigned chores",
+                "Add a chore",
+                "Search for roommate",
+                "Assign a chore to a roommate",
+                "Chores Per Person",
                 "Exit"
             };
 
